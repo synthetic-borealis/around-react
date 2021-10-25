fe4078701a0ded5d5cf175ca63651810da3daac9
Frazonar <snip.coloured@gmail.com>
2021-10-25 13:53:07 +0300

    feat: get user data from API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,31 @@
+import React from 'react';
+import api from '../utils/api';
+
 function Main(props) {
+  const [userName, setUserName] = React.useState('Jacques Cousteau');
+  const [userDescription, setUserDescription] = React.useState('Explorer');
+  const [userAvatar, setUserAvatar] = React.useState('');
+
+  api.getRemoteData().then(([userData, initialCardData]) => {
+    setUserName(userData.name);
+    setUserDescription(userData.about);
+    setUserAvatar(userData.avatar);
+  })
+  .catch(console.log);
+
   return (
     <main>
       <section className="profile">
         <div className="profile__avatar-container">
-          <div className="profile__avatar"></div>
+          <div className="profile__avatar" style={{ backgroundImage: `url(${userAvatar})` }}></div>
           <button className="profile__avatar-button" onClick={props.onEditAvatarClick}></button>
         </div>
         <div className="profile__info">
           <div className="profile__header">
-            <h1 className="profile__name">Jacques Cousteau</h1>
+            <h1 className="profile__name">{userName}</h1>
             <button className="profile__edit-button" type="button" aria-label="Edit" onClick={props.onEditProfileClick}></button>
           </div>
-          <p className="profile__job">Explorer</p>
+          <p className="profile__job">{userDescription}</p>
         </div>
         <button className="add-button" type="button" aria-label="Add" onClick={props.onAddPlaceClick}></button>
       </section>
